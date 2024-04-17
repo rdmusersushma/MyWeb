b1605deee0ce63a02b8cf2d3f1246fd726bc239d
rdmusersushma <marathisushma@gmail.com>
2024-04-17 22:05:24 +0530

    first commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,8 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
 CRISPY_TEMPLATE_PACK ='bootstrap4'
+
+LOGIN_REDIRECT_URL ='blog-home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
